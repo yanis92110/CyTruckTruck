007072c4ec949da1b60e0a60af67facfbd680f11
yanis92110 <126026727+yanis92110@users.noreply.github.com>
2024-01-04 15:03:24 +0100

    Add files via upload

---
+++ test.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		printf("Arguments passés \n");
+		for (int i = 1; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+			printf("----------\n");
+		}
+	}
+	else
+	{
+		printf("Aucun argument n'a été passé\n");
+	}
+	
+	return 0;
+}
